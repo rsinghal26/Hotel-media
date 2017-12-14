a553460ec38a9fe4989a0b000853f66e6136d59a
rsinghal26 <singhalraja.30003@gmail.com>
2017-12-15 01:20:29 +0530

    database update

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
-var express = require("express");
-var app  = express();
-var bodyParser = require("body-parser");
+var express = require("express"),
+    app  = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose");
+    
+//MONGODB---------------------------------------------------    
+mongoose.connect("mongodb://localhost/hotel_app");
+var hotelSchema = new mongoose.Schema({
+    name:String,
+    image:String,
+    description:String
+});
+
+var hotl = mongoose.model("hotel",hotelSchema);
+
+
+
+//MONGODB CLOSE---------------------------------------------------
+
 app.use(bodyParser.urlencoded({extended:true}));
-
 app.set("view engine","ejs");
-
 app.listen(process.env.PORT,process.env.IP,function(){
     console.log("Connected..........");
 });
@@ -13,24 +27,40 @@ app.get("/",function(req,res){
     res.render("landing");
 })
 
-var photos = [
-        {name:"King penguin",image:"http://cdn2.arkive.org/media/93/939EDA21-DD6E-46EE-BD87-64DF9E7FF859/Presentation.Large/King-penguins-allopreening.jpg"},
-        {name:"African grey parrot",image:"http://cdn2.arkive.org/media/9D/9DAA69CF-F5CF-476A-8857-8A1F53897885/Presentation.Large/African-grey-parrot-perched.jpg"},
-        {name:"Kakapo",image:"http://cdn1.arkive.org/media/56/5692B0B2-7BCE-40CB-BB19-D48B085CF32E/Presentation.Large/Kakapo-walking.jpg"},
-        {name:"Andean flamingo",image:"http://cdn2.arkive.org/media/89/89EB2AF8-CD86-4B14-ADF3-D8B9584B95D0/Presentation.Large/Andean-flamingo-flying.jpg"},
-    ]
-app.get("/photos",function(req,res){
-    res.render("photos",{photos:photos});
+app.get("/hotels",function(req,res){
+    hotl.find({},function(err,allHotel){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("hotels",{hotels: allHotel});
+        }
+    });
 })
 
-app.post("/photos",function(req,res){
+app.post("/hotels",function(req,res){
     var caption = req.body.caption;
     var img = req.body.url;
-    var result = {name:caption,image:img};
-    photos.push(result);
-    res.redirect("/photos");
+    var dec = req.body.desc;
+    var result = {name:caption,image:img,description:dec};
+   hotl.create(result,function(err,success){
+        if(err){
+            console.log(err);
+        }else{
+            res.redirect("/hotels");
+        }
+    });
 });
 
-app.get("/photos/addNew",function(req, res) {
+app.get("/hotels/addNew",function(req, res) {
     res.render("addNew.ejs");
+})
+
+app.get("/hotels/:id",function(req, res) {
+     hotl.findById(req.params.id,function(err,fHotel){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("showMore",{hotel: fHotel});
+        }
+    });
 })
